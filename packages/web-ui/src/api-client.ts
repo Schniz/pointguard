ba7f65b26e6ae6e748dcbeced107364d34358726
Gal Schlezinger <gal@spitfire.co.il>
2023-11-15 12:56:07 +0200

    add a header for now

--- packages/web-ui/src/api-client.ts
+++ packages/web-ui/src/api-client.ts
@@ -4,6 +4,11 @@ import { Fetcher } from "openapi-typescript-fetch";
 const apiClient = Fetcher.for<paths>();
 apiClient.configure({
   baseUrl: new URL("/", window.location.href).href.replace(/\/$/, ""),
+  init: {
+    headers: {
+      "x-pointguard": "1",
+    },
+  },
 });
 
 export { apiClient };
